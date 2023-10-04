2ec029656fb75019b0bbef41021291ebb794ba5d
snowmen01 <aphong213@gmail.com>
2023-10-04 21:20:20 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1033,61 +1033,61 @@
 }</code>
                             </pre>
                         </div>
-                        <div class="alert">
-                            <h5 class="heading pb-3">Thông tin người dùng <span class="badge admin">Auth</span>
-                            </h5>
-                            <div class="content pb-3">
-                                <div class="url-container">
-                                    <span class="url">URL:
-                                        {{ config('app.url_local') . '/api/v1/users/show' }}</span>
-                                </div>
-                            </div>
-                            <div class="pb-3">
-                                <span>Method: </span><span class="method-badge get">GET</span>
-                            </div>
-                            <div class="pb-3">
-                                <span>Bearer Token: </span>
+                    </div>
+                    <div class="alert">
+                        <h5 class="heading pb-3">Thông tin người dùng <span class="badge admin">Auth</span>
+                        </h5>
+                        <div class="content pb-3">
+                            <div class="url-container">
+                                <span class="url">URL:
+                                    {{ config('app.url_local') . '/api/v1/users/show' }}</span>
                             </div>
                         </div>
-                        <div class="alert">
-                            <h5 class="heading pb-3">Danh sách vai trò <span class="badge admin">Auth</span>
-                            </h5>
-                            <div class="content pb-3">
-                                <div class="url-container">
-                                    <span class="url">URL: {{ config('app.url_local') . '/api/v1/roles' }}</span>
-                                </div>
-                            </div>
-                            <div class="pb-3">
-                                <span>Method: </span><span class="method-badge get">GET</span>
-                            </div>
-                            <div class="pb-3">
-                                <span>Bearer Token: </span>
-                            </div>
+                        <div class="pb-3">
+                            <span>Method: </span><span class="method-badge get">GET</span>
+                        </div>
+                        <div class="pb-3">
+                            <span>Bearer Token: </span>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
-                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
-                    <div class="flex items-center gap-4">
-                        <a href="#"
-                            class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg>
-                            snowmen
-                        </a>
+                    <div class="alert">
+                        <h5 class="heading pb-3">Danh sách vai trò <span class="badge admin">Auth</span>
+                        </h5>
+                        <div class="content pb-3">
+                            <div class="url-container">
+                                <span class="url">URL: {{ config('app.url_local') . '/api/v1/roles' }}</span>
+                            </div>
+                        </div>
+                        <div class="pb-3">
+                            <span>Method: </span><span class="method-badge get">GET</span>
+                        </div>
+                        <div class="pb-3">
+                            <span>Bearer Token: </span>
+                        </div>
                     </div>
-                </div>
-
-                <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                    FstudioAPI
                 </div>
             </div>
         </div>
+        <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+            <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                <div class="flex items-center gap-4">
+                    <a href="#"
+                        class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                        snowmen
+                    </a>
+                </div>
+            </div>
+
+            <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                FstudioAPI
+            </div>
+        </div>
+    </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/go.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
