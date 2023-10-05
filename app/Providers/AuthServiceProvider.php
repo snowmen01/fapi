e036ec83e733216f5540095ef6b0b8975e089e89
snowmen01 <aphong213@gmail.com>
2023-10-05 21:40:52 +0700

    update

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -16,6 +17,10 @@ class AuthServiceProvider extends ServiceProvider
         Gate::before(function ($user, $ability) {
             $check = $user->hasRole(config('constant.super_admin_id') || config('constant.develop')) ? true : null;
             return $check;
+        });
+
+        Gate::define('viewApiDocs', function (User $user) {
+            return in_array($user->email, ['admin@gmail.com']);
         });
     }
 }
